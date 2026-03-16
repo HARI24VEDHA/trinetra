b7ff72174a35b4ebbbff61ca20c05cb1aaae7cba
Teju3011 <tej301105@gmail.com>
2026-03-17 00:07:36 +0530

    Update flows.py

--- core/flows.py
+++ core/flows.py
@@ -1,13 +1,32 @@
+import pandas as pd
+
+
 def reconstruct_flows(df):
 
-    df["flow"] = (
-        df["src_ip"] +
-        "-" +
-        df["dst_ip"] +
-        "-" +
-        df["protocol"].astype(str)
-    )
+    flows = {}
 
-    flows = df.groupby("flow").size()
+    for _, row in df.iterrows():
 
-    return flows
+        key = (
+            row["src_ip"],
+            row["dst_ip"],
+            row["src_port"],
+            row["dst_port"],
+            row["protocol"]
+        )
+
+        if key not in flows:
+            flows[key] = {
+                "src_ip": row["src_ip"],
+                "dst_ip": row["dst_ip"],
+                "src_port": row["src_port"],
+                "dst_port": row["dst_port"],
+                "protocol": row["protocol"],
+                "packet_count": 0,
+                "bytes": 0
+            }
+
+        flows[key]["packet_count"] += 1
+        flows[key]["bytes"] += row["length"]
+
+    return pd.DataFrame(flows.values())
